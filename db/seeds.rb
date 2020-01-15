25ea739e3f68c828b5055ee338abe729f211ef2e
Student <example@example.com>
2020-01-15 10:08:05 -0800

    seed db manually and through faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Album.destroy_all
+
+album_list = [
+  ["36 Chambers", "rap"],
+  ["Blonde", "smooth rap"],
+  ["Dark Thoughts", "Garage Rock"],
+  ["Fate", "Indie Rock"],
+  ["Fried Chicken and Evil Women", "Honky Tonk"],
+  ["Beethoven Classics", "Classical"],
+  ["Mariah Carey: Christmas", "Soft Rock, Holiday"]
+]
+
+album_list.each do |album, genre|
+  Album.create( name: album, genre: genre )
+end
+
+50.times do |index|
+  albums = []
+  albums.push(Album.create!(name: Faker::Music.album,
+    genre: Faker::Music.genre))
+    albums
+    rand(1..15).times do |song|
+      albums.each do |album|
+        Song.create!(:name => Faker::Music.instrument,
+          :album_id => album.id)
+        end
+      end
+    end
+
+    p "Created #{Album.count} albums"
