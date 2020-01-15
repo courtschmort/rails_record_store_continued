c8a1ffb8b940c56473bb54a90085930463bca839
Student <example@example.com>
2020-01-15 10:08:40 -0800

    add scope and has_many and :through

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,12 @@
 class Album < ApplicationRecord
+  scope :rock, -> { where(genre: "Rock") }
+
+  # Alternate way of writing a class method with scope
+  # def self.rock
+  #   where(genre: "Rock")
+  # end
+
+  has_and_belongs_to_many(:artists)
 
   has_many :songs, dependent: :destroy
 
