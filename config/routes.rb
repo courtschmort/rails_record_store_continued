6f0974367b92b8b0c1b724a11803663db85411e4
Student <example@example.com>
2020-01-22 13:35:35 -0800

    begin adding BCrypt authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   resources :albums do
     resources :songs
   end
+get '/signup' => 'users#new'
+post '/users' => 'users#create'
+
+get '/signin' => 'sessions#new'
+post '/signin' => 'sessions#create'
+get '/signout' => 'sessions#destroy'
 end
